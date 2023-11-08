9af28ca3f3f9d313ab0d622c5c8333751cb660df
Tomaso Zanardi <87314995+Tomaso-Zanardi@users.noreply.github.com>
2023-11-08 16:06:55 +0100

    Add files via upload

---
+++ tracker.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using Palmmedia.ReportGenerator.Core.Parser.Analysis;
+
+public class tracker : MonoBehaviour
+{
+    public GameObject tracked;
+    
+    void Start()
+    {
+        //CreateTextP();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CreateTextP();
+    }
+
+
+    void CreateTextP()
+    {
+        string path = Application.dataPath + "/LoveIsLoveAndLoveIsGay";
+        if (!File.Exists(path))
+        {
+            File.WriteAllText(path,"Tracker \n\n");
+        }
+        string content = "position at time: " + System.DateTime.Now + tracked.transform.position.x + ","+ tracked.transform.position.y + ","+ tracked.transform.position.z + "," + "\n";
+        File.AppendAllText(path,content);
+    }
+}
